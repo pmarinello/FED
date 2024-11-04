658b737d0d7808f6dd828e2481a20963f1a9f250
pmarinello <153516529+pmarinello@users.noreply.github.com>
2024-11-04 15:52:09 +0100

    Final change before exam

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ Nb. Door *open* toe te voegen aan een *details* element kun je deze standaard op
   Rood
 
   #### Je focus:
-  Responsive
+   Ik wilde de pagina responsive maken, echter ben ik uiteindelijk toch overgestapt naar de surface plane. Dit koste toch iet wat minder tijd dan responsive design.
  
 </details>
 
@@ -36,15 +36,17 @@ Nb. Door *open* toe te voegen aan een *details* element kun je deze standaard op
   <summary>uitwerken voor kick-off werkgroep</summary>
 
   ### Je opdracht:
-  Ik ga deze site namaken: (https://www.mossy.earth)
+  Ik ga deze site namaken: (https://www.fastnedcharging.com/en)
 
   #### Screenshot(s) van de eerste pagina (small screen): 
-  hier de naam van de pagina  
-  <img src="images/IMG_4925.PNG" width="375px" alt="Homepage">
+  Fastned Home Page
+
+  <img src="images/mdImages/fastnedHomePage.PNG" width="375px" alt="Homepage">
 
   #### Screenshot(s) van de tweede pagina (small screen):
-  hier de naam van de pagina  
-  <img src="images/IMG_4926.PNG" width="375px" alt="Reforesting Iceland Pagina">
+  Fastned Sustainability and Reporting Page
+
+  <img src="images/mdImages/fastnedCompliancePage.PNG" width="375px" alt="Reforesting Iceland Pagina">
  
 </details>
 
@@ -58,6 +60,10 @@ Nb. Door *open* toe te voegen aan een *details* element kun je deze standaard op
   ### Bevindingen
   Lijst met je bevindingen die in de test naar voren kwamen:
 
+  - Veel beeldspraak / de titels & teksten zijn niet allemaal even makkelijk om te lezen
+  - Geen support voor dark mode of high contrast mode
+  - Geen alt titels bij images
+
 </details>
 
 
@@ -68,13 +74,17 @@ Nb. Door *open* toe te voegen aan een *details* element kun je deze standaard op
   <summary>uitwerken na afloop 3<sup>e</sup> werkgroep</summary>
 
   ### de hele pagina: 
-  <img src="readme-images/dummy-plaatje.jpg" width="375px" alt="breakdown van de hele pagina">
+  <img src="images/mdImages/heroSection.PNG" width="375px" alt="breakdown van de hele pagina - Deel 1">
+  <img src="images/mdImages/appInfoSection.PNG" width="375px" alt="breakdown van de hele pagina - Deel 2">
+  <img src="images/mdImages/gettingStartedSection.PNG" width="375px" alt="breakdown van de hele pagina - Deel 3">
+  <img src="images/mdImages/businessSection.PNG" width="375px" alt="breakdown van de hele pagina - Deel 4">
+  
 
   ### dynamisch deel (bijv menu): 
-  <img src="readme-images/dummy-plaatje.jpg" width="375px" alt="breakdown van een dynamisch deel">
+  <img src="images/mdImages/hamburgerMenu.PNG" width="375px" alt="breakdown van het hamburger menu">
 
   ### wellicht nog een dynamisch deel (bijv filter): 
-  <img src="readme-images/dummy-plaatje.jpg" width="375px" alt="breakdown van nog een dynamisch deel">
+  <img src="images/mdImages/backgroundAnimation.PNG" width="375px" alt="background animatie breakdown">
 
 </details>
 
@@ -88,62 +98,23 @@ Nb. Door *open* toe te voegen aan een *details* element kun je deze standaard op
   <summary>uitwerken voor 1<sup>e</sup> voortgang</summary>
 
   ### Stand van zaken
-  hier dit ging goed & dit was lastig (neem ook screenshots op van delen van je website en code)
+  Ik heb een begin gemaakt aan de HTML en de eerste pagina is bijna af. De CSS en JS ben ik nog niet aan begonnen.
 
 
   ### Agenda voor meeting
-  samen met je groepje opstellen
+  Mijn groepje en ik hebben eigenlijk geen enkele keer onze vragen vooraf doorgenomen omdat we allemaal met andere specifieke vragen zaten.
 
-  | student 1      | student 2          | student 3    | student 4        |
-  | ---            | ---                | ---          | ---              |
-  | dit bespreken  | en dit             | en ik dit    | en dan ik dat    |
-  | en dat ook nog | dit als er tijd is | nog een punt | dit wil ik zeker |
-  | ...            | ...                | ...          | ...              |
+  Voor mij is deze week de vraag of mijn HTML klopt of niet.
 
 
   ### Verslag van meeting
   hier na afloop snel de uitkomsten van de meeting vastleggen
 
-  - punt 1
-  - punt 2
-  - nog een punt
-  - ...
+  - Meer UL gebruiken in plaats van losse a-links of buttons onder elkaar.
+  - Kijk nog eens goed naar je sections / divs
+
 
 </details>
-
-
-
-
-
-## Voortgang 2 (week 3)
-
-<details>
-  <summary>uitwerken voor 2<sup>e</sup> voortgang</summary>
-
-  ### Stand van zaken
-  hier dit ging goed & dit was lastig (neem ook screenshots op van delen van je website en code)
-
-
-  ### Agenda voor meeting
-  samen met je groepje opstellen
-
-  | student 1      | student 2          | student 3    | student 4        |
-  | ---            | ---                | ---          | ---              |
-  | dit bespreken  | en dit             | en ik dit    | en dan ik dat    |
-  | en dat ook nog | dit als er tijd is | nog een punt | dit wil ik zeker |
-  | ...            | ...                | ...          | ...              |
-
-
-  ### Verslag van meeting
-  hier na afloop snel de uitkomsten van de meeting vastleggen
-
-  - punt 1
-  - punt 2
-  - nog een punt
-- ...
-
-</details>
-
 
 
 
@@ -155,8 +126,17 @@ Nb. Door *open* toe te voegen aan een *details* element kun je deze standaard op
 
   ### Bevindingen
   Lijst met je bevindingen die in de test naar voren kwamen (geef ook aan wat er verbeterd is):
+  
+  Wat er verbeterd is:
 
-</details>
+  - Er is een darkmode toegevoegd
+  - Relevante images hebben een alt titel ontvangen
+
+
+  Wat er verslechterd is:
+
+  - toetsenbord navigatie werkt niet (voor zo ver ik weet)
+  </details>
 
 
 
@@ -168,26 +148,19 @@ Nb. Door *open* toe te voegen aan een *details* element kun je deze standaard op
   <summary>uitwerken voor 3<sup>e</sup> voortgang</summary>
 
   ### Stand van zaken
-  hier dit ging goed & dit was lastig (neem ook screenshots op van delen van je website en code)
+  Mooie vooruitgang geboekt, beide HTML pagina's zijn klaar en de CSS voor de Home Page is af. 
 
 
   ### Agenda voor meeting
-  samen met je groepje opstellen
-
-  | student 1      | student 2          | student 3    | student 4        |
-  | ---            | ---                | ---          | ---              |
-  | dit bespreken  | en dit             | en ik dit    | en dan ik dat    |
-  | en dat ook nog | dit als er tijd is | nog een punt | dit wil ik zeker |
-  | ...            | ...                | ...          | ...              |
+  Ik loop compleet vast met de vormgeving van section waarbij twee elementen als een puzzel in elkaar klikken. Ik heb dit eerst met Skew geprobeerd, maar deze methode is helaas niet mogelijk. Echter loop ik ook vast met Clip Path's.
+  Hoe kan ik dit fixen?
 
 
   ### Verslag van meeting
   hier na afloop snel de uitkomsten van de meeting vastleggen
 
-  - punt 1
-  - punt 2
-  - nog een punt
-  - ...
+  - Wellicht de clippath met figma maken en dan de svg code plakken in de HTML
+  - Header menu moet worden uitgewerkt!!
 
 </details>
 
@@ -195,25 +168,30 @@ Nb. Door *open* toe te voegen aan een *details* element kun je deze standaard op
 
 
 
-## Eindgesprek (week 5)
+## Eindgesprek (herkansing)
 
 <details>
   <summary>uitwerken voor eindgesprek</summary>
 
   ### Je uitkomst - karakteristiek screenshots:
-  <img src="readme-images/dummy-plaatje.jpg" width="375px" alt="uitomst opdracht 1">
+  <img src="images/mdImages/homePageResultaat.PNG" width="375px" alt="uitomst opdracht 1">
 
 
   ### Dit ging goed/Heb ik geleerd: 
-  Korte omschrijving met plaatjes
+  De CSS schrijven ging over het algemeen erg goed en ik ben trots op de verschillende resultaten die ik heb behaald. 1 van deze mooi resultaten is de werkende email signup pop-up, die ook checkt of je een naam langer dan 2 karakters hebt ingevoeerd (zonder bijzondere tekens) en of je een valide email hebt ingevuld.
 
-  <img src="readme-images/dummy-plaatje.jpg" width="375px" alt="top">
+  <img src="images/mdImages/emailCheck.PNG" width="375px" alt="top">
 
 
   ### Dit was lastig/Is niet gelukt:
-  Korte omschrijving met plaatjes
+  In het laatste feedback gesprek heb ik van danny mooie en leerzame feedback ontvangen. Wat er namelijk bij mij fout is gelopen is het te lang vast blijven zitten in een code probleem. Tuurlijk is het belangrijk dat je niet direct opgeeft wanneer je tegen een probleem aan loopt, echter is er ook een moment dat je tegen jezelf moet zeggen "en nu door naar het volgende probleem". Dit is iets waar ik flink aan ga werken dit schooljaar en hoop dat bij project tech op de proef te kunnen stellen.
 
-  <img src="readme-images/dummy-plaatje.jpg" width="375px" alt="bummer">
+  Hieronder zie je de vormgeving van de originele Fastned website, helaas is dit mij dus niet gelukt. Al heb ik het met een prima alternatief opgelost. 
+
+  Verder vind ik dat ik mijn code een stuk eleganter kan opmaken, met meer comments tussen de verschillende CSS sections bijvoorbeeld. Dit vergeet ik vaak te doen als ik in een flow zit, maar achteraf alles bijwerken is dan een enorme klus. Ook dit wil ik graag verbeteren aan mijn codeer skills dit jaar.
+
+  <img src="images/mdImages/vormgevingUitdaging.PNG" width="375px" alt="bummer">
+
 </details>
 
 
@@ -223,14 +201,16 @@ Nb. Door *open* toe te voegen aan een *details* element kun je deze standaard op
 ## Bronnenlijst
 
 <details open>
-  <summary>continu bijhouden terwijl je werkt</summary>
 
-  Nb. Wees specifiek ('css-tricks' als bron is bijv. niet specifiek genoeg). 
-  Nb. ChatGpT en andere AI horen er ook bij.
-  Nb. Vermeld de bronnen ook in je code.
 
-  1. bron 1
-  2. bron 2
-  3. ...
+Verre weg de grootste bron die ik heb gebruikt is ChatGPT. Deze bron bestond uit een tal van verschillende chats, waaruit verschillende stukken code zijn gekomen. Helaas ondersteund ChatGPT het delen van chat met daarin bestanden nog niet, waardoor ik deze chats niet direct kan delen.
+Uiteraard laat ik deze maar al te graag zien tijdens het mondeling zo nodig.
+
+  1. Bounding box / scroll animatie - bron : https://chatgpt.com/share/6717c256-0360-8000-9a5c-3812094531e7 
+  2. Email + naam verificatie - bron: stack overflow - onderste comment op de pagina (code komt van persoon met username Gudvit) https://stackoverflow.com/questions/3073850/javascript-regex-test-peoples-name
+  3. Email pop up: https://chatgpt.com/share/6717c256-0360-8000-9a5c-3812094531e7 
+  4. Checkmark animatie: https://developedbyed.com/p/the-ultimate-javascript-animation-course
+  5. Dark theme: https://chatgpt.com/share/6717c256-0360-8000-9a5c-3812094531e7 
+  6. Reduce Motion: https://www.w3.org/WAI/WCAG22/Techniques/css/C39.html
 
 </details>
--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
+// Verre weg de grootste bron die ik heb gebruikt is ChatGPT. Deze bron bestond uit een tal van verschillende chats, waaruit verschillende stukken code zijn gekomen. Helaas ondersteund ChatGPT het delen van chat met daarin bestanden nog niet, waardoor ik deze chats niet direct kan delen.
+// Uiteraard laat ik deze maar al te graag zien tijdens het mondeling zo nodig.
+// 
+
+// https://chatgpt.com/share/6717c256-0360-8000-9a5c-3812094531e7
+
+
+
 const hamburgerIcon = document.querySelector(".hamburgerIcon");
 const headerNav = document.querySelector(".headerNav")
 const kruisje = document.querySelector(".headerNav div > button:nth-child(2)")
@@ -36,24 +44,27 @@ hamburgerIcon.addEventListener("click", toggleMenu);
 
 
 
-// https://developer.mozilla.org/en-US/docs/Web/API/IntersectionObserver
-// https://chatgpt.com/share/6717c256-0360-8000-9a5c-3812094531e7
+
+// Inplaats van section observer gebruik gemaakt van window.innerHeight en .getBoundingClientRect() om vervolgens met een if else statement te checken of het element zich tussen de gewenste margins bevindt.
+// De functie wordt bij iedere scroll opgeroepen
+//
+// Bron en inspiratie voor deze setup kwam van chatGPT https://chatgpt.com/share/6717c256-0360-8000-9a5c-3812094531e7
 
 const yellowBox = document.querySelector('.yellowBox');
 const backgroundBox = document.querySelector('.backgroundBox');
 
-// Function to check if the element is within the middle 75% of the viewport
+
 function checkVisibility() {
     const viewportHeight = window.innerHeight;
 
-    // Calculate the boundaries for the middle 75% of the viewport
+    // De margins voor de middelste 75% van de viewport
     const middleTop = viewportHeight * 0.10;  // 12.5% from the top
     const middleBottom = viewportHeight * 0.85; // 87.5% from the top
 
-    // Get the element's bounding box
+    // De locatie van de box ten opzichte van de viewport
     const rect = backgroundBox.getBoundingClientRect();
 
-    // Check if the element is within the middle 75% of the viewport
+    // Is de box binnen de margins?
     if (rect.top >= middleTop && rect.bottom <= middleBottom) {
         yellowBox.style.transform = 'scale(10)'; // Expand the yellowBox
     } else {
@@ -61,10 +72,10 @@ function checkVisibility() {
     }
 }
 
-// Set up a scroll event listener to continuously check the visibility
+// Roep de functie op bij iedere scroll
 window.addEventListener('scroll', checkVisibility);
 
-// Initial check on page load
+// Check bij iedere keer dat de pagina wordt ingeladen
 checkVisibility();
 
 
@@ -72,9 +83,9 @@ checkVisibility();
 
 
 
-// dark theme 
+// dark theme:  https://chatgpt.com/share/6717c256-0360-8000-9a5c-3812094531e7 
 
-// Function to apply the theme based on system preference
+// Wanneer de pagina wordt geladen checkt deze functie de voorkur van de gebruiker.
 function applyThemeBasedOnSystemPreference() {
     const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
     
@@ -85,7 +96,7 @@ function applyThemeBasedOnSystemPreference() {
     }
 }
 
-// Listen for changes in system theme preference and apply the corresponding theme
+// Wanneer de voorkeur wordt veranderd
 window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
     if (event.matches) {
         document.documentElement.setAttribute('data-theme', 'dark');
@@ -94,15 +105,20 @@ window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', eve
     }
 });
 
-// Initial check to set theme on page load
+// Check bij iedere keer dat de pagina wordt ingeladen
 applyThemeBasedOnSystemPreference();
 
 
+
+
 // Newsletter sign up window
+//
+// Inspiaratie bron voor de onderstaande code was ChatGPT: https://chatgpt.com/share/6717c256-0360-8000-9a5c-3812094531e7
 
 const signUpButton = document.querySelector(".signUpButton");
 const popupOverlay = document.querySelector(".popup-overlay");
 const signupForm = document.querySelector("#signupForm");
+const popupTitle = document.querySelector(".popup h2");
 const emailInput = document.querySelector("#email");
 const successMessage = document.querySelector(".success-message");
 const errorMessage = document.querySelector(".error-message");
@@ -111,6 +127,7 @@ const inputName = document.querySelector("#name");
 
 function openPopup() {
     popupOverlay.classList.remove("hidden");
+    resetForm();
 }
 
 function closePopup() {
@@ -118,14 +135,7 @@ function closePopup() {
     resetForm();
 }
 
-function resetForm() {
-    signupForm.classList.remove("hidden");
-    successMessage.classList.add("hidden");
-    errorMessage.classList.add("hidden");
-    errorMessage2.classList.add("hidden");
-    emailInput.classList.remove("valid", "invalid");
-    signupForm.reset();
-}
+
 
 function validateName() {
     const nameValue = inputName.value;
@@ -136,11 +146,6 @@ function validateName() {
         inputName.classList.remove("invalid");
         errorMessage.classList.add("hidden");
     } 
-    
-    // else if (nameValue.length > 0 && nameValue.length < 2 && nameValue.length == focus) {
-    //     inputName.classList.remove("valid");
-    //     inputName.classList.remove("invalid");
-    // }
     
     else {
         inputName.classList.remove("valid");
@@ -187,35 +192,73 @@ function removeEmailColor() {
     emailInput.classList.remove("invalid");
 }
 
+function showSuccessMessage() {
+    signupForm.classList.add("hideSignupForm");
+    successMessage.classList.remove("hidden");
+    popupTitle.textContent = "Thank you!";
+    setTimeout(closePopup, 2000);
+}
+
 function submit(event) {
     event.preventDefault();
 
-    const emailValue = emailInput.value;
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const nameValue = inputName.value;
 
-    if (!emailPattern.test(emailValue)) {
-        emailInput.classList.add("invalid");
+    // bron: stack overflow - onderste comment op de pagina (code komt van persoon met username Gudvit) https://stackoverflow.com/questions/3073850/javascript-regex-test-peoples-name
+    const namePattern = /^[ a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœŒ'`'\-]+$/;
+
+    const emailValue = emailInput.value;
+    const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+
+    // als alles klopt
+    if (emailPattern.test(emailValue) && inputName.value.length > 1 && namePattern.test(nameValue)) {
+        emailInput.classList.remove("invalid");
+        errorMessage.classList.add("hidden");
+        errorMessage2.classList.add("hidden");
+        showSuccessMessage();
+    }
+
+
+    // als geen van beide kloppen
+    else if (nameValue.length < 2 && !namePattern.test(nameValue) && !emailPattern.test(emailValue)) {
+        errorMessage.classList.remove("hidden");
+        errorMessage2.classList.remove("hidden");
+        return;
+    }
+
+    // als naam niet klopt
+    else if (emailPattern.test(emailValue) && inputName.value.length < 2 && !namePattern.test(nameValue)) {
+        errorMessage2.classList.add("hidden");
         errorMessage.classList.remove("hidden");
         return;
     }
 
-    emailInput.classList.remove("invalid");
-    showSuccessMessage();
+    // als email niet klopt
+    else if (inputName.value.length > 1 && namePattern.test(nameValue) && !emailPattern.test(emailValue)) {
+        errorMessage.classList.add("hidden");
+        errorMessage2.classList.remove("hidden");
+        return;
+    }
+
+    
 }
 
-function showSuccessMessage() {
-    signupForm.classList.add("hidden");
-    successMessage.classList.remove("hidden");
 
-    setTimeout(closePopup, 2000);
-}
 
 
 
 //Checkbox animation fill
+//
+// Bron: De code is geinspireerd op een les uit een cursus van DevelopedbyEd. De code is echter aangepast naar mijn eigen wensen en stijl.
+// link naar cursus: https://developedbyed.com/p/the-ultimate-javascript-animation-course
+//
+// Specifiek was dit een les over Forms, waar ook de checkmark animatie werd behandeld.
+
 const checkbox = document.querySelector(".checkbox");
 const promoContainer = document.querySelector(".promoContainer");
 const checkboxContainer = document.querySelector(".checkbox-container");
+
 
 const tl2 = gsap.timeline({
   defaults: { duration: 0.5, ease: "Power2.easeOut" },
@@ -234,47 +277,69 @@ promoContainer.addEventListener("click", () => {
     checkbox.checked = false;
 
     tl2.to(".checkbox-fill", { top: "100%" });
+    tl2.to(".checkbox-label", { color: "black" }, "<");
     tl2.fromTo(
       tickMarkPath,
       { strokeDashoffset: 0 },
       { strokeDashoffset: pathLength },
       "<50%"
     );
-    tl2.to(".checkbox-label", { color: "#c5c5c5" }, "<");
+    
     checkboxContainer.classList.remove("checked");
   } 
   
   else {
     tl2.to(".checkbox-fill", { top: "0%" });
+    tl2.to(".checkbox-label", { color: "green" }, "<");
     tl2.fromTo(
       tickMarkPath,
       { strokeDashoffset: pathLength },
       { strokeDashoffset: 0 },
       "<50%"
     );
-    tl2.to(".checkbox-label", { color: "#6391e8" }, "<");
+    
     checkbox.checked = true;
     checkboxContainer.classList.add("checked");
   }
 });
 
 
-
-
-
-
-
-
-
+function resetForm() {
+    signupForm.classList.remove("hidden");
+    successMessage.classList.add("hidden");
+    errorMessage.classList.add("hidden");
+    errorMessage2.classList.add("hidden");
+    emailInput.classList.remove("valid", "invalid");
+    signupForm.classList.remove("hideSignupForm");
+    signupForm.reset();
+    checkbox.checked = false;
+    checkboxContainer.classList.remove("checked");
+    tl2.to(".checkbox-fill", { top: "100%" });
+    tl2.to(".checkbox-label", { color: "black" });
+    popupTitle.textContent = "Sign up for our newsletter";
+}
 
 // Event Listeners
 signUpButton.addEventListener("click", openPopup);
 signupForm.addEventListener("submit", submit);
-emailInput.addEventListener("input", validateEmail); // Real-time validation
+emailInput.addEventListener("input", validateEmail); 
 emailInput.addEventListener("focusout", validateEmailFocusout); 
-inputName.addEventListener("focusout", validateName); // Real-time validation
+inputName.addEventListener("focusout", validateName); 
 inputName.addEventListener("focusin", removeNameColor);
 emailInput.addEventListener("focusin", removeEmailColor);
+window.addEventListener("touchend", (event) => {
+    if (event.target === popupOverlay) {
+        closePopup();
+    }
+}   );
+
+window.addEventListener("click", (event) => {
+    if (event.target === popupOverlay) {
+        closePopup();
+    }
+}   );
+
+
 
 
 
